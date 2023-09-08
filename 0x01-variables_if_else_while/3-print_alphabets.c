20b58b35d2186b9f31601fc9e3fd99d34b5d8188
Lamyae125 <lamyaebokoko@gmail.com>
2023-09-08 14:58:01 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char C;
+	char d;
+
+	C = 'a';
+	d = 'A';
+	while
+		(C <= 'Z') {
+			putchar(C);
+			C++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
